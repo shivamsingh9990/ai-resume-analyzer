730379edc70bb065e893415bd3eaad67e1f91528
shivam <shivamsinghmaurya9@gmail.com>
2025-11-24 14:06:03 +0530

    cloud torage, PDF-Image conversion,key-vlue storage and ai feedback

---
+++ app/utils.ts
@@ -0,0 +1,11 @@
+export function formatSize(bytes: number):string{
+    if(bytes===0)return  ' 0 bytees';
+    const k=1024;
+    const sizes=['bytes','KB','MB','GB','TB',];
+
+    const i:number=Math.floor(Math.log(bytes)/Math.log(k));
+    return parseFloat((bytes/Math.pow(k,i)).toFixed(2))+' '+sizes[i];
+
+}
+
+export const generateUUID=()=> crypto.randomUUID();
